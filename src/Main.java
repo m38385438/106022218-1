147a29543e5deb8411f9e5b28af4e275364ffc8f
m38385438 <m38385438@gmail.com>
2017-09-18 15:22:00 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("學號:106022218");
+        System.out.println("系級:行動商務與多媒體應用學系一年級");
+        System.out.println("姓名:楊子億");
+        System.out.println("時間:2017/09/18");
+        System.out.print("地點:\"I319\"");
+    }
+}
